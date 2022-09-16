2dd6c795e723ef209ccd90642b1541533c6ad406
Serhan <srhncetin4@gmail.com>
2022-09-16 12:26:08 +0300

    propTypes adjusted

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,10 @@
+import PropTypes from "prop-types"
 export default function User({ name, surName, isLoggedIn, age, friends }) {
   return (
     <>
       <h1>
         {isLoggedIn
-          ? `Merhaba ${name} ${surName}, ${age}`
+          ? `Merhaba ${name} ${surName}, (${age})`
           : "Lütfen Giriş Yapınız."}
       </h1>
 
@@ -12,4 +13,12 @@ export default function User({ name, surName, isLoggedIn, age, friends }) {
       ))}
     </>
   );
+}
+
+User.propTypes = {
+    name:PropTypes.string.isRequired,
+    surName:PropTypes.string.isRequired,
+    isLoggedIn:PropTypes.bool.isRequired,
+    age:PropTypes.number.isRequired,
+    friends:PropTypes.array.isRequired
 }
